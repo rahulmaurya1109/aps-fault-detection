dd190632844c6aee3bbc86b053c0aeaf8af9f20f
rahulmaurya1109 <rahulmmaurya751@gmail.com>
2022-12-05 19:48:40 +0530

    updating the sensor setup and requirements file

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import find_packages,setup
 
+def get_requirements():
+    pass
 
-setup()
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="rahulmaurya1109",
+    author_email="rahulmaurya751@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements(),
+)
